a336b76e179f99189272c46c397a3f6c88fc83ab
rbian12 <rabin.kunn@gmail.com>
2024-04-26 12:25:46 +0545

    changes

--- claire/c-r-button.js
+++ claire/c-r-button.js
@@ -1,11 +1,25 @@
+// conditional rendering button js
 window.addEventListener('DOMContentLoaded', function() {
   const mainButtons = document.querySelectorAll('a[data-main="true"]');
-  const currentURL = window.location.href;
+  const currentHost = window.location.host;
+   // Get the waitlist popup div
+   const waitlistPopup = document.querySelector('.waitlist_popup');
 
-  if (currentURL === 'https://claire-web.realpha.com/') {
+  if (currentHost === 'claire-web.realpha.com' || currentHost === 'claire.realpha.com') {
     mainButtons.forEach(function(button) {
       button.href = '#';
       button.querySelector('.button-text').textContent = 'Join Waitlist';
+      button.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+        waitlistPopup.style.display = 'flex'; // Show the waitlist popup
+      });
     });
   }
+
+    // Function to close the popup defined globally if needed elsewhere
+  window.closePopup = function() {
+      if (waitlistPopup) {
+          waitlistPopup.style.display = 'none';
+      }
+  };
 });
